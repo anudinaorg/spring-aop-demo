68e4f63dc08ef7b1d369cd5ae2440172f1ae266e
Administrator <Administrator@AnandLaptop>
2016-07-14 21:59:52 -0400

    updated

---
+++ src/main/java/com/ado/springaop/domain/Employer.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package com.ado.springaop.domain;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Employer {
+
+	private int emplId;
+	private String empFirstName;
+	private String empLastName;
+	private String empAddress;
+	private String zipCode;
+	/**
+	 * @return the emplId
+	 */
+	public int getEmplId() {
+		return emplId;
+	}
+	/**
+	 * @param emplId the emplId to set
+	 */
+	public void setEmplId(int emplId) {
+		this.emplId = emplId;
+	}
+	/**
+	 * @return the empFirstName
+	 */
+	public String getEmpFirstName() {
+		return empFirstName;
+	}
+	/**
+	 * @param empFirstName the empFirstName to set
+	 */
+	public void setEmpFirstName(String empFirstName) {
+		this.empFirstName = empFirstName;
+	}
+	/**
+	 * @return the empLastName
+	 */
+	public String getEmpLastName() {
+		return empLastName;
+	}
+	/**
+	 * @param empLastName the empLastName to set
+	 */
+	public void setEmpLastName(String empLastName) {
+		this.empLastName = empLastName;
+	}
+	/**
+	 * @return the empAddress
+	 */
+	public String getEmpAddress() {
+		return empAddress;
+	}
+	/**
+	 * @param empAddress the empAddress to set
+	 */
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+	/**
+	 * @param zipCode the zipCode to set
+	 */
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+}
